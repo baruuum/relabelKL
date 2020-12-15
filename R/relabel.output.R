f0c6fb4dabfc1378c4b26fd7e230cef079b9e746
baruuum <baruuum@gmail.com>
2020-12-15 00:56:24 -0500

    add permutation for 4-dim arrays; still need to be unit tested

--- R/relabel.output.R
+++ R/relabel.output.R
@@ -193,7 +193,7 @@ relabelTRUE = function(x, x_true, log_p = TRUE, renormalize = FALSE, nthreads = 
 #' @param perms a \code{S} times \code{K} matrix of permutations needed to
 #'   transformed the raw MCMC output to the permuted output, e.g., the 
 #'   \code{perms} element from running \code{relabelMCMC}.
-#' @param what one of "rows", "cols," or "both". Determines what dimensions
+#' @param what one of "rows", "cols," "both", or "matrix." Determines what dimensions
 #'   are permuted. For two-dimensional arrays with unequal dimensions
 #'   (i.e., rectangular matrices), the columns are always permuted.
 #' @return returns \code{x} permuted according to \code{perms}
@@ -204,58 +204,85 @@ relabelTRUE = function(x, x_true, log_p = TRUE, renormalize = FALSE, nthreads = 
 #'   number of posterior draws and will permute either the columns or the
 #'   rows of the sub-array which is of dimensions \code{N}*\code{K}. If
 #'   \code{x} is a matrix, the function assumes that \code{nrow(x) == S} 
-#'   and will permute the columns.
+#'   and will permute the columns. When the \code{what} argument is set to "matrix," 
+#'   it is assumed that the entered array is of dimension \code{S}\*\code{K}\*\code{L}\*\code{L}, and permutes the whole \code{L}\*\code{L} matrix across the \code{K} classes for each posterior draw. 
 #' @export
-permuteMCMC = function(x, perms, what) {
+permuteMCMC = function(x, perms, what = "cols") {
     
-    p.dim = match.arg(what, 
-                      c("rows", "cols", "both"),
-                      several.ok = FALSE)
+    p_dim = match.arg(
+        what, 
+        c("rows", "cols", "both", "matrix"),
+        several.ok = FALSE
+    )
     
     if (!is.array(x))
         stop("x has to be an array (permuteMCMC)")
     
-    n.dim = length(dim(x))
+    n_dim = length(dim(x))
     S = nrow(perms)
     K = ncol(perms)
     
-    if ((n.dim == 3) & ((dim(x)[3] != K) | (dim(x)[1] != S)))
-        stop("size mismatch (permuteMCMC)")
+    if (n_dim == 2) {
+        
+        if ((dim(x)[2] != K) | (dim(x)[1] != S))
+            stop("size mismatch (permuteMCMC)")
+        
+        if (p_dim != "cols")
+            stop("for two dimensional arrays, 'what' has to be set to 'cols'")
+
+    }
     
-    if ((n.dim == 2) & ((dim(x)[2] != K) | (dim(x)[1] != S)))
-        stop("size mismatch (permuteMCMC)")
+    if (n_dim == 3) {
+        
+        if ((dim(x)[3] != K) | (dim(x)[1] != S))
+            stop("size mismatch (permuteMCMC)")
+        
+        if (p_dim == "both" && dim(x)[2] != dim(x)[3]) 
+            stop("parameter is not a square matrix but 'both' was specified")    
+    }
     
-    if ((n.dim == 2) & p.dim %in% c("rows", "both")) 
-        stop("if x is a two-dimensional array, make sure that the rows are the draws and the columns the labels")
-    
-    if ((n.dim == 3) & (p.dim == "both") & (dim(x)[2] != dim(x)[3])) 
-        stop("parameter are not square matrices but 'both' was specified")
+    if (n_dim == 4) {
+      
+        if (p_dim != "matrix")
+            stop("Only available option for the 'what' argument is 'matrix' for 4 dim arrays")
+        
+        stopifnot(
+            dim(x)[2] == K,
+            dim(x)[3] == dim(x)[4]
+        )
+            
+    } 
         
     
-    if (n.dim == 3) {
+    if (n_dim == 2) {
+        
+        res = t(sapply(1:S, function(s) { x[s, perms[s, ]] }))
+        
+    } else if (n_dim == 3) {
         
         res = switch(
-            p.dim,
+            p_dim,
             "rows" = lapply(1:S, function(s) {
-                            x[s, perms[s, ], ]
-                        }),
+                x[s, perms[s, ], ]
+            }),
             "cols" = lapply(1:S, function(s) {
-                            x[s, , perms[s, ]]
-                        }),
+                x[s, , perms[s, ]]
+            }),
             "both" = lapply(1:S, function(s) {
-                            x[s, perms[s, ], perms[s, ]]
-                        })
+                x[s, perms[s, ], perms[s, ]]
+            })
         )
         
         res = aperm(simplify2array(res), c(3, 1, 2))
         
-    } else if (n.dim == 2) {
+    } else if (n_dim == 4) {
         
-        res = t(sapply(1:S, function(s) { x[s, perms[s, ]] }))
+        res = lapply(1:S, function(s) x[s, perms[s, ], ,])
+        res = aperm(simplify2array(res), c(4, 1:3))
         
     } else {
 
-        stop("n.dim not 2 or 3 (permuteMCMC)")
+        stop("n_dim must be smaller than 5 (permuteMCMC)")
         
     }
     
@@ -264,4 +291,5 @@ permuteMCMC = function(x, perms, what) {
     attr(res, "org.att") = attr(x, "org.att")
     
     res
+    
 }
