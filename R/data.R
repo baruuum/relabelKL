9a61e349b70834370664685f03f4401266ee5cb9
baruuum <40509370+baruuum@users.noreply.github.com>
2024-01-15 13:56:35 -0500

    bugfix & use githubaction

--- R/data.R
+++ R/data.R
@@ -4,7 +4,7 @@
 #' blockmodel to simulated data. 
 #' 
 #' @format A \code{stanfit} object:
-#' \describe{
+#' \itemize{
 #'   \item{}{Model has two parameters: the membership vectors \code{pi} and the image matrix \code{theta}}
 #'   \item{}{Two MCMC chains were run on simulated data using NUTS, each sampling 500 samples after 500 iterations of warmup}
 #'   \item{}{The Stan-code can be accessed by typing \code{mmsbm@stanmodel}}
